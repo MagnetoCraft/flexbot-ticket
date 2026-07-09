f9574f1f9aaf103a2ab9e1827ea1b998da6ffd50
Alexandre <athibaul@enssat.fr>
2026-07-10 01:58:12 +0200

    Adding JFA

--- src/main/java/fr/tioslexe/flexbotticket/Main.java
+++ src/main/java/fr/tioslexe/flexbotticket/Main.java
@@ -1,7 +1,15 @@
 package fr.tioslexe.flexbotticket;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
+
+    private static String TOKEN = System.getenv("TOKEN");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JDA jda = JDABuilder.createDefault(TOKEN).build();
+
+//        jda.awaitReady();
     }
 }
